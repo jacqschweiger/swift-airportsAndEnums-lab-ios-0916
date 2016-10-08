d9085239fc83f7b5265358e1356ef83f788e476b
jacqschweiger <jacqschweiger@gmail.com>
2016-10-08 14:09:06 -0400

    Done

--- AirportsAndEnums/ViewController.swift
+++ AirportsAndEnums/ViewController.swift
@@ -44,6 +44,13 @@ class ViewController: UIViewController {
     var feltTemp: FeltTemp = .none
     var windDirection: WindDirection = .V
     var weatherCondition: WeatherCondition = .none
+    var airportCode: AirportCode? {
+        didSet {
+            if let airportCode = airportCode, let airportDictionary = airportDictionary {
+                airportStatus = airportDictionary[airportCode.rawValue] as? AirportStatus
+            }
+        }
+    }
     
     // Airport Status Dictionary
     var airportDictionary: NSDictionary? {
@@ -51,7 +58,7 @@ class ViewController: UIViewController {
             if let airportDict = airportDictionary {
                 let sortedKeys = (airportDict.allKeys as! [String]).sorted(by: <)
                 if let code = sortedKeys.first {
-                    
+                    airportCode = AirportCode(rawValue: code)
                 }
             }
         }
@@ -76,6 +83,8 @@ class ViewController: UIViewController {
         setUpView()
         airportDictionary = AirportStatus.getTestDataDictionary()
         
+        print(airportCode)
+        
     }
 }
 
@@ -90,6 +99,25 @@ extension ViewController {
     }
     
     // Airport Code Enum
+    enum AirportCode: String {
+        case ATL, DFW, JFK, LAX, ORD
+        
+        mutating func next() {
+            switch self {
+            case .ATL:
+                self = .DFW
+            case .DFW:
+                self = .JFK
+            case .JFK:
+                self = .LAX
+            case .LAX:
+                self = .ORD
+            case .ORD:
+                self = .ATL
+            }
+        }
+        
+    }
 
     
     // Weather Condition Enum
@@ -269,6 +297,8 @@ extension ViewController {
     // Change status for view
     func changeStatusWithAnimation() {
         if statusReceived {
+            
+            airportCode?.next()
             
             UIView.transition(with: view, duration: 0.5, options: .transitionFlipFromRight, animations: nil, completion: nil)
         }
